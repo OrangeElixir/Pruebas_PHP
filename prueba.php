54500b0a088e453a4be428a8245339b3db38fa61
OrangeElixir <1334577@senati.pe>
2022-10-29 17:05:11 -0500

    p1_php

---
+++ prueba.php
@@ -0,0 +1,10 @@
+<?php
+echo "<center>";
+echo "<h1><strong>Número Random</strong></h1>";
+echo "<p>Actualiza la página para otro número</p>";
+echo "<p>RANDOM</p>";
+echo rand(1,15);
+echo "<br>";
+echo "<button onclick='location.reload()'>Otro Número</button>";
+echo "</center>"
+?>
